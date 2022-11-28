36c35477c46675fc50917a9280ce326e2f6ea251
Ahmet Ceyran <ahmetceyran00@gmail.com>
2022-11-28 16:29:53 +0300

    Clean Data

--- code_cleaner.py
+++ code_cleaner.py
@@ -1,7 +1,19 @@
 import pandas as pd
+import numpy as np
+import csv
 
-df = pd.read_csv('AllDatas.csv')
+df = pd.read_csv('AllDatasV1.csv')
 
-print(df.shape)
+df = df.drop(['web-scraper-order', 'web-scraper-start-url', 'teams-href', 'playerprofile-href', 'playername'], axis = 1)
 
-print(df.isnull().sum())
+df['birthdate'] = df['birthdate'].str.replace("                                                                    "," ")
+
+df.head()
+
+df.shape
+
+df.info()
+
+df.to_csv("C:\\Users\\ahmet\\GitHub\\DataMiningProject\\AllDatasV3.csv")
+
+
